515ea1f174f02346ffd8e9674fe4c14bd07b4440
Ulrik <ulrik.strid@outlook.com>
2020-05-11 07:37:20 +0200

    Split out esy build to speed up actions

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,14 +1,9 @@
-echo Building esy docker with version $1
-docker build --rm -f "Dockerfile.esy" --build-arg ESY_VERSION=$1 -t reasonnative/esy:$1 "."
+echo Building ocaml image with OCaml version $1
+docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 -t reasonnative/ocaml:$1 "."
 echo Pushing image...
-docker push reasonnative/esy:$1
+docker push reasonnative/ocaml:$1
 
-echo Building ocaml image with OCaml version $2
-docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$2 -t reasonnative/ocaml:$2 "."
+echo Building web image with OCaml version $1
+docker build --rm -f "Dockerfile.web" --build-arg OCAML_VERSION=$1 -t reasonnative/web:$1 "."
 echo Pushing image...
-docker push reasonnative/ocaml:$2
-
-echo Building web image with OCaml version $2
-docker build --rm -f "Dockerfile.web" --build-arg OCAML_VERSION=$2 -t reasonnative/web:$2 "."
-echo Pushing image...
-docker push reasonnative/web:$2
+docker push reasonnative/web:$1
