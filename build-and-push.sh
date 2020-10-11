33b94c2ba76d7206b77219f8ce0e634e559e3cc5
Ulrik <ulrik.strid@outlook.com>
2020-10-11 15:32:05 +0200

    Latest esy

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo Building ocaml image with OCaml version $1
 docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 -t reasonnative/ocaml:$1 "."
 echo Pushing image...
