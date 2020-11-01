159cf5a87280164c2ef5f8b993bc6d3a070bd8e2
Ulrik <ulrik.strid@outlook.com>
2020-11-01 14:21:49 +0100

    Add nightly builds

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo Building ocaml image with OCaml version $1
-docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 -t reasonnative/ocaml:$1 "."
+echo Building ocaml image with OCaml version $1 and esy $2
+docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 --build-arg ESY_VERSION=$2 -t reasonnative/ocaml:$1 "."
 echo Pushing image...
-docker push reasonnative/ocaml:$1
+docker push reasonnative/ocaml:$1-$2
 
 echo Building web image with OCaml version $1
-docker build --rm -f "Dockerfile.web" --build-arg OCAML_VERSION=$1 -t reasonnative/web:$1 "."
+docker build --rm -f "Dockerfile.web" --build-arg OCAML_VERSION=$1 --build-arg ESY_VERSION=$2 -t reasonnative/web:$1-$2 "."
 echo Pushing image...
-docker push reasonnative/web:$1
+docker push reasonnative/web:$1-$2
