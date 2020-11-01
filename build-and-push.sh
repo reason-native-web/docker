316b9dd9cdfe5360bb7a9d2795cfca0ff39cf275
Ulrik <ulrik.strid@outlook.com>
2020-11-01 14:30:47 +0100

    Fix tag on ocaml image

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo Building ocaml image with OCaml version $1 and esy $2
-docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 --build-arg ESY_VERSION=$2 -t reasonnative/ocaml:$1 "."
+docker build --rm -f "Dockerfile.ocaml" --build-arg OCAML_VERSION=$1 --build-arg ESY_VERSION=$2 -t reasonnative/ocaml:$1-$2 "."
 echo Pushing image...
 docker push reasonnative/ocaml:$1-$2
 
